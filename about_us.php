3ac65a75ad29a52db81ce27e48b9d7e01ec926df
Eric Poblete <113760743+ericerikerickk@users.noreply.github.com>
2022-10-27 11:42:49 +0800

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,221 @@
+<?php
+include("includes/head.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<title>About Us</title>
+    <style type="text/css">
+
+         .ART1 {
+         margin: 110px 0px 0px -220px;
+          font-size: 90px;
+          font-family: "Yu Gothic UI";
+          color: rgb( 45, 112, 213 );
+          text-align: center;
+          position: absolute;
+          top: -15px;
+          width: 215px;
+          height: 35px;
+          z-index: 19;
+        }
+
+        ._BAY1 {
+            margin: 110px 0px 5px 180px;
+          font-size: 90px;
+          font-family: "Yu Gothic UI";
+          color: white;
+          text-align: center;
+          position: absolute;
+          top: -15px;
+          width: 215px;
+          height: 35px;
+          z-index: 19;
+        }
+
+        .Albay_1 {
+          margin: 250px 0px 0px 10px;
+          font-size: 30px;
+          font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+           z-index: 3;
+        }
+
+        .Original_1 {
+          margin: 250px 0px 0px 190px;
+          font-size: 30px;
+         font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+           z-index: 3;
+        }
+
+        .Artworks_1{
+
+          margin: 250px 0px 0px 325px;
+          font-size: 30px;
+          font-family: "MingLiU_HKSCS-ExtB";
+          color: #f5f5f5;
+          text-align: left;
+          position: absolute;
+          top: -55px;
+          width: 215px;
+          height: 35px;
+          z-index: 3;
+        }
+          .border1{
+          border-radius: 8px;
+        float: left;
+        border: 1px solid none;
+        background-color: #112233;
+        padding: 150px 490px 0px 250px;
+        margin-left: 250px;
+        margin-top: 100px;
+
+    }
+     .border2{
+          border-radius: 8px;
+        float: left;
+        border: 1px solid none;
+        background-color: #112233;
+        padding: 420px 0px 0px 1160px;
+        margin-left: 50px;
+        margin-top: -20px;
+        margin-bottom: 0px;
+        position: absolute;
+        top:250px;
+        bottom: 0px;
+        opacity: 0.5;
+
+    }
+    .text1{
+            position: absolute;
+            color: rgb(255, 255, 255);
+            margin: 10px 5px 105px 5px;
+            text-align: justify;
+            font-family: "Yu Gothic UI Light";
+            font-weight: normal;
+            font-size: 20px;
+            top: 40px;
+            left: 50px;
+            right: 50px;
+            z-index: 3;
+
+        }
+         .head-the {
+          font-size: 30px;
+          font-family: "Yu Gothic UI Light";
+          color: rgb( 45, 112, 213 );
+          position: absolute;
+         margin: 700px 0px 100px 70px;
+
+          }
+        .head-staff{
+          font-size: 30px;
+         font-family: "Yu Gothic UI Light";
+          color: rgb( 00, 00, 00 );
+          position: absolute;
+           margin:700px 0px 100px 130px;
+          z-index: 19;
+        }
+        .hr{
+            position: absolute;
+            border: 1px solid #2d70d5;
+            top: 827px;
+            width: 1050px;
+            left: 73px;
+        }
+        .pics{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -770px;
+          height: 230px;
+          width: 220px;
+        }
+        .pics2{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -510px;
+          height: 230px;
+          width: 220px;
+        }
+         .pics3{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -255px;
+          height: 230px;
+          width: 220px;
+        }
+          .pics4{
+          border-radius: 180px;
+          background-color: #1E5D9A;
+          float: left;
+          position: relative;
+          margin:750px 0px 100px -5px;
+          height: 230px;
+          width: 220px;
+        }
+          .text2{
+            position: absolute;
+            color: black;
+            text-align: justify;
+            font-family: "Yu Gothic UI Light";
+            font-weight: bold;
+            font-size: 18px;
+           top:1020px;
+           left: 152px;
+            z-index: 3;
+            margin-bottom: 100px;
+        }
+        .text3{
+            position: absolute;
+            color: #112233;
+            margin: 10px 5px 105px 5px;
+            text-align: justify;
+            font-family: "Yu Gothic UI Light";
+            font-weight: normal;
+            font-size: 15px;
+            top: 1050px;
+            left: 180px;
+            z-index: 3;
+        }
+
+    </style>
+
+<body>
+    <div class="border1">
+          <h1 class="ART1">DASHING </h1>
+          <h1 class="_BAY1">+ART </h1>
+          <h2 class="Albay_1">Philippines </h2>
+          <h2 class="Original_1">Original </h2>
+          <h2 class="Artworks_1">Artworks </h2>
+  </div>
+  <div class="border2">
+
+
+<p class="text1" >Created this year, October 2022, DASHING+ART Philippines Original Artworks Gallery, located in the UCLM, has quickly become one of the Humanista's premier contemporary and modern art partners.
+
+<br><br>Dashing+Art Philippines Original Artworks Gallery maintains four keys areas of focus: exhibitions, education, residency and consultation.
+
+<br><br>It is curated exhibitions provide audiences with an opportunity to experience different approaches and execution styles of art. The artists, both local and international, are often provided the opportunity to exhibit the work they have created at DASHING+ART at the end of their residency. DASHING+ART also provides qualified art consultation services for its discerning collectors, building on and enhancing existing collections, as well as helping establish new collections.
+
+<br><br>Artworks can be created with a very wide range of techniques, and many artists even combine several techniques and mediums in their works.</p><br><br><br><br><br>
+ </div>
+<br><br><br><br>
+
+</body>
+</html>
